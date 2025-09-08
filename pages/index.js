061816ce31fd6d956eb77081780cf2b3815c3c26
Lucas Cardoso <54423255+LucasBCardoso@users.noreply.github.com>
2025-09-08 21:43:50 +0000

    rolled again

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,27 @@
+import Image from "next/image";
+
 function Home() {
-    //return <h1>Task 1 - Tabnews</h1>
-    return (
-        <>
-            <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                minHeight: '100vh',
-                textAlign: 'center'
-            }}>
-                <img 
-                    src="https://media.tenor.com/x62iRrI_b2QAAAAC/rick-roll-rick-rolled.gif"
-                    alt="Rick Roll GIF"
-                    style={{
-                        width: '200px', 
-                        height: '200px',
-                        objectFit: 'cover'
-                    }}
-                />
-                <p style={{marginTop: '20px', fontSize: '18px'}}>Nunca vou te abandonar.</p>
-            </div>
-        </>
-    );
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '100vh',
+      textAlign: 'center'
+    }}>
+      <Image
+        src="https://media.tenor.com/x62iRrI_b2QAAAAC/rick-roll-rick-rolled.gif"
+        alt="Rick Roll GIF"
+        width={200}
+        height={200}
+        priority // força o pré-carregamento imediato
+      />
+      <p style={{ marginTop: '20px', fontSize: '18px' }}>
+        Nunca vou te abandonar.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
