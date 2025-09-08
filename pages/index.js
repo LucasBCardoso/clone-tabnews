84f4f72a227b621cf1ff183db6aa8848e10a3ca8
Lucas Cardoso <54423255+LucasBCardoso@users.noreply.github.com>
2025-09-08 21:39:01 +0000

    Centralize gif, set size to 200x200px and add text below

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,21 @@ function Home() {
     //return <h1>Task 1 - Tabnews</h1>
     return (
         <>
-            <div className="tenor-gif-embed" data-postid="25206401" data-share-method="host" data-aspect-ratio="1" data-width="100%">
-                <a href="https://tenor.com/view/rick-roll-rick-rolled-rick-astley-never-gonna-give-you-up-hd-gif-25206401">Rick Roll Rick Rolled GIF</a>
-                from <a href="https://tenor.com/search/rick+roll-gifs">Rick Roll GIFs</a>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: '100vh',
+                textAlign: 'center'
+            }}>
+                <div className="tenor-gif-embed" data-postid="25206401" data-share-method="host" data-aspect-ratio="1" data-width="200px" style={{width: '200px', height: '200px'}}>
+                    <a href="https://tenor.com/view/rick-roll-rick-rolled-rick-astley-never-gonna-give-you-up-hd-gif-25206401">Rick Roll Rick Rolled GIF</a>
+                    from <a href="https://tenor.com/search/rick+roll-gifs">Rick Roll GIFs</a>
+                </div>
+                <p style={{marginTop: '20px', fontSize: '18px'}}>Nunca vou te abandonar.</p>
+                <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
             </div>
-            <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
         </>
     );
 }
