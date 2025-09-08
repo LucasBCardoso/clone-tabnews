8944e0c3813a24f94feca8b308f1483275b2bf78
Lucas Cardoso <54423255+LucasBCardoso@users.noreply.github.com>
2025-09-08 21:35:31 +0000

    rick

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Task 1 - Tabnews</h1>
+    //return <h1>Task 1 - Tabnews</h1>
+    return (
+        <>
+            <div className="tenor-gif-embed" data-postid="25206401" data-share-method="host" data-aspect-ratio="1" data-width="100%">
+                <a href="https://tenor.com/view/rick-roll-rick-rolled-rick-astley-never-gonna-give-you-up-hd-gif-25206401">Rick Roll Rick Rolled GIF</a>
+                from <a href="https://tenor.com/search/rick+roll-gifs">Rick Roll GIFs</a>
+            </div>
+            <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+        </>
+    );
 }
 
 export default Home;
